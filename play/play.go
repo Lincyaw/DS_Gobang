e117592c9c0af5fe3517affdf9952d8e448eb112
lincyaw <814750204@qq.com>
2021-03-27 21:42:36 +0800

    能够顺利入座，下棋。但实现交替下棋时存在bug

--- play/play.go
+++ play/play.go
@@ -1,18 +1,54 @@
 package play
 
-import "log"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+)
 
+type pData struct {
+	Type string `json:"type"`
+	pos  `json:"position"`
+}
+type pos struct {
+	X int `json:"x"`
+	Y int `json:"y"`
+}
 
-func Play(data string)(re map[string]string, err error)  {
+func Play(chessboard [][]int, data string, user int) (map[string]string, error) {
 	log.Println(data)
-	//记录当前数据
-	//判断输赢
-	//if true {
-	//	data.Type = "play"
-	//}else{
-	//	//修改user
-	//	//todo
-	//	data.Type = "win"
-	//}
-	return
+	d := pData{
+		Type: "",
+		pos:  pos{},
+	}
+	re:=make(map[string]string)
+	err := error(nil)
+	err = json.Unmarshal([]byte(data), &d)
+	if err != nil {
+		log.Println("无法解析的报文：", data, "error:", err)
+		return re, err
+	}
+	fmt.Println("struct", d)
+
+	height, width := len(chessboard), len(chessboard[0])
+	if d.X < 0 || d.X >= width || d.Y >= height || d.Y < 0 {
+		re["message"] = "不在棋盘范围内"
+		return re, err
+	}
+	if chessboard[d.X][d.Y] == 0 {
+		chessboard[d.X][d.Y] = user
+	} else {
+		re["message"] = "已经在这里下过棋了"
+		return re, errors.New("重复下棋")
+	}
+
+	// todo: 判断是否有赢家或平手
+	for i:=0;i<len(chessboard);i++{
+		for j:=0;j<len(chessboard[0]);j++{
+			fmt.Print(chessboard[i][j])
+		}
+		fmt.Println("")
+	}
+	return re,nil
 }
--- play/seat.go
+++ play/seat.go
@@ -12,45 +12,57 @@ var MsgNewName = map[string]string{
 	"new": "",
 }
 
-func Seat(clients map[*websocket.Conn]*User, conn *websocket.Conn, msg string) (re map[string]string, err error) {
+func Seat(clients map[*websocket.Conn]*User, conn *websocket.Conn, msg string) (map[string]string, error) {
 	data := map[string]int{"position": 0}
+	err := error(nil)
+	re := make(map[string]string)
 	err = json.Unmarshal([]byte(msg), &data)
 	log.Println(data, msg)
 	if err != nil {
 		log.Println("无法解析的报文：", msg, "error:", err)
-		return
+		return re, err
 	}
 	a, b := 0, 0
-	for k, v := range clients {
+	for _, v := range clients {
 		switch v.Type {
 		case 1:
 			a = 1
 		case 2:
 			b = 1
 		}
-		if k == conn {
-			err = errors.New("已经坐下了")
-			return
-		}
+		//if k == conn {
+		//	err = errors.New("已经坐下了")
+		//	return
+		//}
 	}
 	if a == 1 && b == 1 {
-		err = errors.New("坐满了")
-		return
+		return re, errors.New("已经坐满了")
 	}
+
 	if a == 0 {
-		clients[conn].Type = 1
+		if data["position"] == 1 {
+			clients[conn].Type = 1
+		} else {
+			re["message"] = "这里已经被坐了"
+			return re, errors.New("这里已经被坐了")
+		}
 		a = 1
 		re["message"] = clients[conn].Name + "执白子！"
 	} else if b == 0 {
-		clients[conn].Type = 2
+		if data["position"] == 2 {
+			clients[conn].Type = 2
+		} else {
+			re["message"] = "这里已经被坐了"
+			return re, errors.New("这里已经被坐了")
+		}
 		b = 1
 		re["message"] = clients[conn].Name + "执黑子！"
 	}
 	if a == 1 && b == 1 { //坐下后坐满了，开局
-		//begain()
+		//begin()
 		fmt.Println("开局")
 	}
-	return
+	return re, nil
 }
 func ChangeName(user *User, msg string) (re map[string]string, err error) {
 	data := map[string]string{"newName": ""}
